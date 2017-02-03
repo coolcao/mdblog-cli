adcb390ae0a23e2d0f033c3629b8e50246bc4059
coolcao <me@coolcao.com>
2017-02-03 17:48:19 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## mdblog命令行工具
+mdblog命令行工具，用于生成mdblog特定格式的md文件。
+
+### 安装
+
+* `npm link`:链接命令
+
+### 使用说明：
+
+* `mdblog new/n file.md`:第一个参数new/n为新建，new或n都可以，file.md为新建文件名称
+* `mdblog --help`:帮助说明命令
---
+++ index.js
@@ -0,0 +1,64 @@
+#! /usr/bin/env node
+
+const fs = require('fs');
+
+let command = process.argv[2];
+let currentPath = process.cwd();
+
+let existFile = function (path) {
+    let exit = false;
+    try{
+        let fileStat = fs.statSync(path);
+        if(fileStat){
+            exit = true;
+        }
+    }catch(err){
+        if(err.code !== 'ENOENT'){
+            exit = true;
+        }
+    }
+    return exit;
+}
+
+let newFile = function () {
+    let file = process.argv[3];
+    let fullPath = currentPath + '/' + file + '.md';
+    let template = `<!--
+{
+    "title":"${file}",
+    "data":${new Date()},
+    "tag":[]
+}
+-->`;
+
+    if(existFile(fullPath)){
+        console.log(`${fullPath} 文件已存在`);
+    }else{
+        console.log(`new file ${fullPath}`);
+        fs.writeFileSync(fullPath,template);
+    }
+}
+
+let help = function () {
+    console.log('Usage: mdblog [command] [arg]');
+    console.log('command:');
+    console.log('\t new|n : new markdown file. [argv] is the file name.');
+    console.log('example:');
+    console.log('\t mdblog n hello : create the new file hello.md');
+}
+
+switch(command) {
+    case 'new':
+        newFile();
+        break;
+    case 'n':
+        newFile();
+        break;
+    case '--help':
+        help();
+        break;
+    default:
+        console.log('nothing to do');
+}
+
+
